dedbc2e9ffe00115f5966e02f9317481438ab6c6
Alejandro <alejandro.omar.rizzuto@gmail.com>
2021-03-23 20:22:21 -0300

    database connection comentada

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-import pyodbc 
+#import pyodbc 
 import DBConst as cte
 import log
 
@@ -7,16 +7,16 @@ class DataBase():
 
     logger = log.logger
 
-    def getConnection(self):
-        """Método para conectarse a la base."""
-        try:
-            conn = pyodbc.connect(cte.CONNECTION)
-            cursor = conn.cursor()
-            cursor.execute("SELECT @@version;")     # Execute query to check connectivity
-            return conn
-        except Exception as e:
-            self.logger.error("Error - getConnection: {}".format(e))
-            return False
+    # def getConnection(self):
+    #     """Método para conectarse a la base."""
+    #     try:
+    #         conn = pyodbc.connect(cte.CONNECTION)
+    #         cursor = conn.cursor()
+    #         cursor.execute("SELECT @@version;")     # Execute query to check connectivity
+    #         return conn
+    #     except Exception as e:
+    #         self.logger.error("Error - getConnection: {}".format(e))
+    #         return False
 
     def saveRecord(self,rec,key):
         """Save a new record."""
@@ -24,23 +24,24 @@ class DataBase():
         last_name = rec['last_name']
         email = rec['email']
         
-        conn = self.getConnection()         # Connect to database
+        # conn = self.getConnection()         # Connect to database
         
-        if conn != False:
-            try:
-                cursor = conn.cursor()        
-                VALUES=[name,last_name,email,key]
-                cursor.execute(cte.QRY_SAVE_NEW_RECORD, VALUES)
-                conn.commit()
-                conn.close()
-                self.logger.info("Record has been succesfully loaded!")
-            except Exception as e:
-                self.logger.error("Error - Could not insert the record: {}".format(e))
-        else:
-            self.logger.error("Error - Could not stablish connection.")
+        # if conn != False:
+        #     try:
+        #         cursor = conn.cursor()        
+        #         VALUES=[name,last_name,email,key]
+        #         cursor.execute(cte.QRY_SAVE_NEW_RECORD, VALUES)
+        #         conn.commit()
+        #         conn.close()
+        #         self.logger.info("Record has been succesfully loaded!")
+        #     except Exception as e:
+        #         self.logger.error("Error - Could not insert the record: {}".format(e))
+        # else:
+        #     self.logger.error("Error - Could not stablish connection.")
 
 
-            ## Para pruebas ####################################
+        ## Para pruebas ####################################
+        if True: # para comentar la parte de DB
             """Saving information in txt file instead using database just for tests."""
             with open('archivo.txt','a') as file_BBDD:
                 values=' '.join([name, last_name, email, key])
@@ -48,24 +49,25 @@ class DataBase():
 
     def getKey(self,key):
         """Get key from database."""
-        conn = self.getConnection()
+        # conn = self.getConnection()
         
-        if conn != False:
-            try:
-                cursor = conn.cursor()        
-                cursor.execute(cte.QRY_GET_KEY,key)
-                row = cursor.fetchall()
-                conn.close()
-                self.logger.info("Key found!")
-                return True if row[0][0] is not "" else False
-            except Exception as e:
-                self.logger.error("Error - Could not found the key: {}".format(e))
-                return False
-        else:
-            self.logger.error("Error - Could not stablish connection.")
-            #return False
+        # if conn != False:
+        #     try:
+        #         cursor = conn.cursor()        
+        #         cursor.execute(cte.QRY_GET_KEY,key)
+        #         row = cursor.fetchall()
+        #         conn.close()
+        #         self.logger.info("Key found!")
+        #         return True if row[0][0] is not "" else False
+        #     except Exception as e:
+        #         self.logger.error("Error - Could not found the key: {}".format(e))
+        #         return False
+        # else:
+        #     self.logger.error("Error - Could not stablish connection.")
+        #     #return False
 
             ## Para pruebas ####################################
+        if True: # para comentar la parte de DB
             """Saving information in txt file instead using database just for tests."""
             with open('archivo.txt','r') as file_BBDD:
                 for line in file_BBDD:
@@ -75,23 +77,24 @@ class DataBase():
 
     def checkUser(self, rec):
         """Check if user exist in database."""
-        conn = self.getConnection()
+        # conn = self.getConnection()
         
-        if conn != False:
-            try:
-                cursor = conn.cursor()        
-                cursor.execute(cte.QRY_CHECK_USER,rec['email']) # Check the user with his email
-                row = cursor.fetchall()
-                conn.close()
-                return True if row[0][0] is not "" else False
-            except Exception as e:
-                self.logger.error("Error - Could not found the key: {}".format(e))
-                return False
-        else:
-            self.logger.error("Error - Could not stablish connection.")
-            #return False
+        # if conn != False:
+        #     try:
+        #         cursor = conn.cursor()        
+        #         cursor.execute(cte.QRY_CHECK_USER,rec['email']) # Check the user with his email
+        #         row = cursor.fetchall()
+        #         conn.close()
+        #         return True if row[0][0] is not "" else False
+        #     except Exception as e:
+        #         self.logger.error("Error - Could not found the key: {}".format(e))
+        #         return False
+        # else:
+        #     self.logger.error("Error - Could not stablish connection.")
+        #     #return False
 
             ## Para pruebas ####################################
+        if True: # para comentar la parte de DB
             """Saving information in txt file instead using database just for tests."""
             with open('archivo.txt','r') as file_BBDD:
                 for line in file_BBDD:
@@ -101,23 +104,24 @@ class DataBase():
 
     def returnKey(self, rec):
         """Get the key for the user."""
-        conn = self.getConnection()
+        # conn = self.getConnection()
         
-        if conn != False:
-            try:
-                cursor = conn.cursor()        
-                cursor.execute(cte.QRY_RETURN_KEY,rec['email']) # Check the user with his email
-                row = cursor.fetchall()
-                conn.close()
-                return row[0][0]
-            except Exception as e:
-                self.logger.error("Error - Could not found the key: {}".format(e))
-                return False
-        else:
-            self.logger.error("Error - Could not stablish connection.")
-            #return False
+        # if conn != False:
+        #     try:
+        #         cursor = conn.cursor()        
+        #         cursor.execute(cte.QRY_RETURN_KEY,rec['email']) # Check the user with his email
+        #         row = cursor.fetchall()
+        #         conn.close()
+        #         return row[0][0]
+        #     except Exception as e:
+        #         self.logger.error("Error - Could not found the key: {}".format(e))
+        #         return False
+        # else:
+        #     self.logger.error("Error - Could not stablish connection.")
+        #     #return False
 
             ## Para pruebas ####################################
+        if True: # para comentar la parte de DB
             """Saving information in txt file instead using database just for tests."""
             with open('archivo.txt','r') as file_BBDD:
                 for line in file_BBDD:
